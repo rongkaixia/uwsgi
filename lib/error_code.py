953f1a7b72c12b2f975efd68037e30650889c4a4
rongkaixia <rongkaixia@gmail.com>
2017-07-17 19:25:04 +0800

    using int error code

--- lib/error_code.py
+++ lib/error_code.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 # encoding : utf-8
 
-OK = "0"
-PARAM_ERROR = "10001"
-SIGN_ERROR = "10002"
-SYSTEM_ERROR = "19999"
+OK = 0
+PARAM_ERROR = 10001
+SIGN_ERROR = 10002
+SYSTEM_ERROR = 19999
